294c17904c76cd59f4354728a57a50164da67031
Ellen <ellenguimaraes@gmail.com>
2019-09-16 16:58:58 +0000

    js

--- static/js/application.js
+++ static/js/application.js
@@ -49,3 +49,52 @@ function saveNewUser(login, userid, username, useremail) {
       console.log('Successful login: ' + response);
     });
 }
+
+function roundToTwo(num)
+{
+    return +(Math.round(num + "e+2")  + "e-2");
+ }
+
+function checkboxes()
+    {
+     var modalContent = document.getElementById('modalContentContainer');
+     var inputElems = document.getElementsByTagName("input"),
+      count = 0;
+
+      for (var i=0; i<inputElems.length; i++) {
+        if (inputElems[i].type == "checkbox" && inputElems[i].checked == true){
+            count++
+            total= roundToTwo(count*4.2);
+        }
+      }
+     modalContent.innerHTML = total+"%";
+ }
+
+document.addEventListener("DOMContentLoaded", function()
+{
+       const box = document.querySelectorAll(".modal");
+       M.Modal.init(box, {
+           opacity:0.6,
+           inDuration:250,
+           outDuration:250,
+           dismissible: true,
+
+       });
+ });
+
+ //sections
+
+ function hideAllPages() {
+  document.getElementById("introductionPage").style.display = "none";
+  document.getElementById("instructionsPage").style.display = "none";
+  document.getElementById("startPage").style.display = "none";
+
+}
+function showinstructionsPage() {
+  hideAllPages();
+  document.getElementById("instructionsPage").style.display = "block";
+}
+function showStartPage() {
+  hideAllPages();
+  document.getElementById("startPage").style.display = "block";
+}
